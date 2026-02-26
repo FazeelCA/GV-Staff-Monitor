973d6e8a7d8e1f034bc6d7dc7f924eb157a28693
FazeelCA <fazeelazeez@gmail.com>
2026-02-26 05:49:55 +0530

    fix(screenshot): Fix rust borrow checker error and move windows-capture into target_os=windows table

--- frontend/src-tauri/src/capture/windows.rs
+++ frontend/src-tauri/src/capture/windows.rs
@@ -14,33 +14,28 @@ use std::sync::mpsc::{channel, Sender};
 
 #[cfg(target_os = "windows")]
 pub fn capture_desktop() -> Result<Vec<u8>, String> {
-
     struct CaptureHandler {
         sender: Sender<Vec<u8>>,
     }
 
     impl GraphicsCaptureApiHandler for CaptureHandler {
-
         type Flags = Sender<Vec<u8>>;
         type Error = Box<dyn std::error::Error + Send + Sync>;
 
-        fn new(
-            ctx: Context<Self::Flags>,
-        ) -> Result<Self, Self::Error> {
+        fn new(ctx: Context<Self::Flags>) -> Result<Self, Self::Error> {
             Ok(Self { sender: ctx.flags })
         }
 
         fn on_frame_arrived(
             &mut self,
             frame: &mut Frame,
-            mut control: InternalCaptureControl,
+            control: InternalCaptureControl,
         ) -> Result<(), Self::Error> {
+            let width = frame.width();
+            let height = frame.height();
 
             let mut buffer = frame.buffer()?;
             let nopadding = buffer.as_nopadding_buffer()?;
-
-            let width = frame.width();
-            let height = frame.height();
 
             let mut rgb = Vec::with_capacity((width * height * 3) as usize);
 
@@ -52,13 +47,12 @@ pub fn capture_desktop() -> Result<Vec<u8>, String> {
 
             let mut jpeg = Vec::new();
 
-            image::codecs::jpeg::JpegEncoder::new_with_quality(&mut jpeg, 40)
-                .encode(
-                    &rgb,
-                    width,
-                    height,
-                    image::ColorType::Rgb8.into(),
-                )?;
+            image::codecs::jpeg::JpegEncoder::new_with_quality(&mut jpeg, 40).encode(
+                &rgb,
+                width,
+                height,
+                image::ColorType::Rgb8.into(),
+            )?;
 
             let _ = self.sender.send(jpeg);
 
